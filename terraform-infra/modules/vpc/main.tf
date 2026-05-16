a4fdc1714b2275b978922325828ac8a6f14cba57
Omprakash <omprakashsurwase1508@gmail.com>
2026-05-16 21:25:51 +0530

    added terraform-infra project

---
+++ terraform-infra/modules/vpc/main.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
